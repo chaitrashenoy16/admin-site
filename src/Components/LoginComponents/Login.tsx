74d92ba32f850e51c2a34019d5feff2ae456a5d2
ESOLUTIONS\cshenoy <chaitra.shenoy@Trivium-esolutions.com>
2023-06-02 09:37:58 +0530

    login changes in UI

--- src/Components/LoginComponents/Login.tsx
+++ src/Components/LoginComponents/Login.tsx
@@ -1,32 +1,39 @@
-import { useState } from "react";
 import ReactDOM from "react-dom";
 import { useNavigate } from "react-router-dom";
 import Alert from "./Alert";
+import { useState } from "react";
+import axios from "axios";
 
 const Login = () => {
   const navigate = useNavigate();
-  let [uname, setUname] = useState("");
-  let [pwd, setPwd] = useState("");
+  const [login, setLogin] = useState("");
+  let [username, setUname] = useState("");
+  let [password, setPwd] = useState("");
   const doLogin = () => {
-    if (uname === "" || pwd === "") {
+    if (username === "" || password === "") {
       alert("Username or password can't be blank");
       return;
     }
-    if (uname === "admin" && pwd === "admin") {
-      localStorage.setItem("uname", uname);
-      localStorage.setItem("Isauth", "true");
-      navigate("/dashboard/");
-    } else {
-      ReactDOM.render(
-        <Alert>Login is not successful please try again...</Alert>,
-        document.getElementById("root")
-      );
-    }
+    axios
+      .post("http://localhost:5024/api/login", {
+        username,
+        password,
+      })
+      .then((res) => {
+        if (res.data) {
+          localStorage.setItem("uName", res.data.userName);
+          localStorage.setItem("Isauth", "true");
+          navigate("/dashboard/");
+        } else {
+          alert("Username or password invalid");
+        }
+      });
   };
   const handleReset = () => {
     setUname("");
     setPwd("");
   };
+
   return (
     <div>
       <div className="mb-3">
@@ -38,7 +45,7 @@ const Login = () => {
           className="form-control"
           id="usernameText"
           placeholder="Enter username"
-          value={uname}
+          value={username}
           onChange={(e) => setUname(e.target.value)}
         ></input>
       </div>
@@ -52,7 +59,7 @@ const Login = () => {
           className="form-control"
           id="passwordText"
           placeholder="Enter password"
-          value={pwd}
+          value={password}
           onChange={(e) => setPwd(e.target.value)}
         ></input>
       </div>
